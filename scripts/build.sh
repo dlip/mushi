5591bca267ead44c986de9bfbd513a00158fed9e
Dane Lipscombe <dane.lipscombe@planpay.com>
2023-08-31 18:00:46 +1000

    Separate left and right cases

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,4 +4,5 @@ set -euo pipefail
 npm exec ergogen -- .
 cp output/pcbs/mushi.kicad_pcb kicad/
 # for i in output/cases/*.jscad; do npm exec openjscad -- "$i" -of stla; done
-npm exec openjscad -- output/cases/case.jscad -of stla -o case/case.stl
+npm exec openjscad -- output/cases/caseLeft.jscad -of stla -o case/caseLeft.stl
+npm exec openjscad -- output/cases/caseRight.jscad -of stla -o case/caseRight.stl
